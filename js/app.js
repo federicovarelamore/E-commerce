62fc3fdeab526b03b92b99260d9a4c4b6c89a368
federicovarelamore <85002323+federicovarelamore@users.noreply.github.com>
2022-06-03 11:18:50 -0300

    new function: form validation

--- js/app.js
+++ js/app.js
@@ -1,22 +1,48 @@
-const ImagenModal = document.querySelector("#modal-image");
+const imagenModal = document.querySelector("#modal-image");
 
-ImagenModal.addEventListener("show.bs.modal", function (event) {
-  const enlace = event.relatedTarget;
-  const rutaImagen = enlace.getAttribute("data-bs-image");
+if (imagenModal) {
+  imagenModal.addEventListener("show.bs.modal", function (event) {
+    const enlace = event.relatedTarget;
+    const rutaImagen = enlace.getAttribute("data-bs-image");
 
-  // Construir la IMG
-  const imagen = document.createElement("IMG");
-  imagen.src = `img/${rutaImagen}.jpg`;
+    // Construir la IMG
+    const imagen = document.createElement("IMG");
+    imagen.src = `img/${rutaImagen}.jpg`;
 
-  imagen.classList.add("img-fluid");
-  imagen.alt = "Imagen Galería";
+    imagen.classList.add("img-fluid");
+    imagen.alt = "Imagen Galería";
 
-  // Inyectar en el HTML
-  const contenidoModal = document.querySelector(".modal-body");
-  contenidoModal.appendChild(imagen);
-});
+    // Inyectar en el HTML
+    const contenidoModal = document.querySelector(".modal-body");
+    contenidoModal.appendChild(imagen);
+  });
 
-ImagenModal.addEventListener("hidden.bs.modal", () => {
-  const contenidoModal = document.querySelector(".modal-body");
-  contenidoModal.lastChild.remove();
-});
+  imagenModal.addEventListener("hidden.bs.modal", () => {
+    const contenidoModal = document.querySelector(".modal-body");
+    contenidoModal.lastChild.remove();
+  });
+}
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
